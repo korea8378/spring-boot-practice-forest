90f29f1c75bd6b441f8ec6b756bb476697e4877a
korea8378 <korea8378@naver.com>
2019-03-04 17:46:11 +0900

    [#7][ADD] jdbcTemplate을 이용한 studentCRUD

---
+++ src/main/java/bom/spring/practice/domain/Subject.java
@@ -0,0 +1,18 @@
+package bom.spring.practice.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+
+    private int id;
+
+    private String name;
+}
---
+++ src/main/java/bom/spring/practice/dto/RequestStudentDto.java
@@ -0,0 +1,16 @@
+package bom.spring.practice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestStudentDto {
+    private String name;
+
+    private int age;
+}
